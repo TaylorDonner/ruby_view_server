7c0bbd7338a5b675ff50bda63ee1a628d7d0ec51
Tim Preston <134735@gmail.com>
2013-06-25 11:42:12 -0500

    J&T third additions. altered default layout

--- block_example.rb
+++ block_example.rb
@@ -2,7 +2,9 @@
 ## functions can take in a block similar to the way Array#each can
 #   i.e. [1,2,3].each {|x| x + 1}
 
-## here we are providing a function called `wrap_around_contents` when a block is passed to it, it will yield to the block
+## here we are providing a function called
+ # `wrap_around_contents` when a block is passed to it, 
+ # it will yield to the block
 def wrap_around_contents
   "hey your contents =>| #{yield} |<= have been wrapped"
 end
