aa7c9b6b7ea733df951822975c131712f298fe32
Tim Preston <134735@gmail.com>
2013-06-25 11:10:29 -0500

    Josh and Taylor's first changes

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,14 @@
 require 'erb'
 
 x = 42
+s = ["kung fu", "panda", "bear" ]
+hash = {:name => "Taylor", :computer => "Mac"}
+
 template = ERB.new "The value of x is: <%= x %>"
+
+template2 = ERB.new "The value of s is: <%= s %>"
+
+template3 = ERB.new "The hash equals: <%= hash %>"
 puts template.result(binding)
+puts template2.result(binding)
+puts template3.result(binding)
